47dbe1a605647e4a018b2ab7afd9de83908dbad9
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-09-03 01:45:03 +0100

    feat: Added swagger and express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const port = process.env.PORT || 3000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
+  });
+  app.use(cookieParser());
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('FintechX Stock Exchange')
+    .setDescription('REST API Services to get quote for a stock symbol')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+  Logger.log(`Server started on http://localhost:${port}, Boostrap`);
 }
 bootstrap();
