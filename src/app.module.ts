6ddafa1113790cfdf04435fe10508962bb1295ae
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-09-03 01:44:05 +0100

    chore: set up the database module and imported it in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './db/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
+@Global()
 export class AppModule {}
