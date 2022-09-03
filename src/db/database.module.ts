74e41c225c4c03d9f373edacd8884c1b782ef721
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-09-03 10:45:20 +0100

    chore: database setup

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -1,8 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.provider';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get(
+          'DB_USERNAME',
+        )}:${configService.get(
+          'DB_PASSWORD',
+        )}@cluster0.fkmvzao.mongodb.net/stock-exchange`,
+        retryAttempts: 2,
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}
