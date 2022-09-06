e0f6ec08a7522b229d09f00fa20a647e86392b7a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-09-06 15:28:48 +0100

    chore: implemented swagger

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/api', app, document);
   await app.listen(port);
   Logger.log(`Server started on http://localhost:${port}, Boostrap`);
 }
